d199626c07eccabb769a125b84b55c723f84f1a4
zen <duzhenquan@eastmoney.com>
2021-08-30 10:56:37 +0800

    zendu   2021-08-30 10:56:37

--- init.sh
+++ init.sh
@@ -4,9 +4,15 @@
 #tree -N >> $file_name
 #echo "\`\`\`" >> $file_name
 
+export https_proxy=127.0.0.1:7776
+export http_proxy=127.0.0.1:7776
+
+rm .*un~*
 git add .
 msg='zendu   '`date "+%Y-%m-%d %H:%M:%S"`;
 git commit -m "${msg}"
 git push 
 
+export https_proxy=
+export http_proxy=
 
